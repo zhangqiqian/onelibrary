6bca44e8b944e18c3f26b4d3dbd7c92165ca82f3
zhangqiqian <zhangqiqian2012@gmail.com>
2015-01-23 11:23:51 +0800

    add log config and logo css

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -24,7 +24,7 @@ return array(
 
     /* URL配置 */
     'URL_CASE_INSENSITIVE' => true, //默认false 表示URL区分大小写 true则表示不区分大小写
-    'URL_MODEL'            => 3, //URL模式
+    'URL_MODEL'            => 2, //URL模式
     'VAR_URL_PARAMS'       => '', // PATHINFO URL参数变量
     'URL_PATHINFO_DEPR'    => '/', //PATHINFO URL分割符
 
@@ -46,4 +46,9 @@ return array(
     //扩展函数库
     "LOAD_EXT_FILE"     => '',
     'LOAD_EXT_CONFIG'   => '',
+
+    'LOG_RECORD'            =>  false,   // 默认不记录日志
+    'LOG_TYPE'              =>  'File', // 日志记录类型 默认为文件方式
+    'LOG_LEVEL'             =>  'EMERG,ALERT,CRIT,ERR',// 允许记录的日志级别
+    'LOG_EXCEPTION_RECORD'  =>  false,    // 是否记录异常信息日志
 );
