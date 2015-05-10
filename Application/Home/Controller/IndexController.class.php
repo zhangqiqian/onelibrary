6e00844764139f65303fc1ca1821ed43e967f7f4
zhangqiqian <zhangqiqian2012@gmail.com>
2015-05-11 01:02:19 +0800

    finish home index messages and detail.

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -9,6 +9,7 @@
 
 namespace Home\Controller;
 use Think\Controller;
+use Common\Model\MessageModel;
 
 /**
  * 前台首页控制器
@@ -16,5 +17,25 @@ use Think\Controller;
 class IndexController extends HomeController {
     public function index(){
         $this->display();
+    }
+
+    public function messages_by_category(){
+        $category_id = I('category_id', 0, 'intval');
+
+        $mMessage = new MessageModel();
+        $messages = $mMessage->get_messages_by_category($category_id);
+        $this->ajaxReturn(array('errno' => 0, 'result' => $messages));
+    }
+
+    public function message_detail(){
+        $message_id = I('message_id', 0, 'intval');
+
+        $mMessage = new MessageModel();
+        $message = $mMessage->get_message($message_id);
+        $categories = C('MESSAGE_CATEGORIES');
+        $message['category'] = $categories[$message['category']];
+
+        $this->assign('message', $message);
+        $this->display('Index/message_detail');
     }
 }
