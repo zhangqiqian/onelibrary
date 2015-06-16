e26c643e59440cfb244b311b3579a3d37156e3fd
zhangqiqian <zhangqiqian2012@gmail.com>
2015-06-17 02:29:51 +0800

    register api

--- Application/Api/Controller/PublicController.class.php
+++ Application/Api/Controller/PublicController.class.php
@@ -70,4 +70,53 @@ class PublicController extends Controller {
         $verify->entry(1);
     }
 
+    /**
+     * @param string $username
+     * @param string $password
+     * @param string $repassword
+     * @param string $email
+     */
+    public function register($username = '', $password = '', $repassword = '', $email = ''){
+        if(!C('USER_ALLOW_REGISTER')){
+            $this->ajaxReturn(array('errno' => 1, 'errmsg' => '注册已关闭'));
+        }
+        if(IS_POST){ //注册用户
+            /* 检测密码 */
+            if($password != $repassword){
+                $this->ajaxReturn(array('errno' => 1, 'errmsg' => '密码和重复密码不一致'));
+            }
+
+            /* 调用注册接口注册用户 */
+            $User = new UserApi;
+            $uid = $User->register($username, $password, $email);
+            if(0 < $uid){ //注册成功
+                $this->ajaxReturn(array('errno' => 1, 'errmsg' => 'Success to sign up'));
+            } else { //注册失败，显示错误信息
+                $this->ajaxReturn(array('errno' => 1, 'errmsg' => $this->showRegError($uid)));
+            }
+        }
+    }
+
+    /**
+     * 获取用户注册错误信息
+     * @param  integer $code 错误编码
+     * @return string        错误信息
+     */
+    private function showRegError($code = 0){
+        switch ($code) {
+            case -1:  $error = '用户名长度必须在16个字符以内！'; break;
+            case -2:  $error = '用户名被禁止注册！'; break;
+            case -3:  $error = '用户名被占用！'; break;
+            case -4:  $error = '密码长度必须在6-30个字符之间！'; break;
+            case -5:  $error = '邮箱格式不正确！'; break;
+            case -6:  $error = '邮箱长度必须在1-32个字符之间！'; break;
+            case -7:  $error = '邮箱被禁止注册！'; break;
+            case -8:  $error = '邮箱被占用！'; break;
+            case -9:  $error = '手机格式不正确！'; break;
+            case -10: $error = '手机被禁止注册！'; break;
+            case -11: $error = '手机号被占用！'; break;
+            default:  $error = '未知错误';
+        }
+        return $error;
+    }
 }
