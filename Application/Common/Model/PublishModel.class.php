4d2463fc4e2b3fda33653a5b60372d2a0e2d7bd2
zhangqiqian <zhangqiqian2012@gmail.com>
2015-12-16 06:32:15 +0800

    change simlarity to 1%

--- Application/Common/Model/PublishModel.class.php
+++ Application/Common/Model/PublishModel.class.php
@@ -139,7 +139,7 @@ class PublishModel extends MongoModel{
         $map['status']  = 0;
         $map['priority']  = array('$gte' => $priority);
         if($notification == 1){
-            $map['similarity']  = array('$gte' => 80);
+            $map['similarity']  = array('$gte' => 1);
         }
         $map['publish_time']  = array('$lte' => $now);
         $map['expire_time']  = array('$gte' => $now);
