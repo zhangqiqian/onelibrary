3bdff8d3d21fda045645079fcbbe6a2924204791
zhangqiqian <zhangqiqian2012@gmail.com>
2015-12-17 01:14:48 +0800

    add crontab controller.

---
+++ Application/Admin/Controller/CrontabController.class.php
@@ -0,0 +1,18 @@
+<?php
+// +----------------------------------------------------------------------
+// | OneLibrary [ DO IT NOW! ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2014 niko All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: zhangqiqian <43874051@qq.com>
+// +----------------------------------------------------------------------
+
+namespace Admin\Controller;
+use Think\Controller;
+
+class CrontabController extends Controller {
+
+    public function test(){
+        echo "OK\n";
+    }
+}
